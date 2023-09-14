4e569e2e014837b4bd21b97da95de4f20283eecb
Danil Vorontsov <rojerr@MacBook-Air-Danil.local>
2023-09-14 22:10:11 +0600

    added features of  chat, new user, left user

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import EmojiPicker from 'emoji-picker-react';
+
+import icon from '../images/emoji.svg';
+import styles from '../styles/Chat.module.css';
+import Messages from './Messages';
 
 const socket = io.connect('http://localhost:5001');
 
 const Chat = () => {
+  const [state, setState] = useState([]);
   const { search } = useLocation();
-  const [params, setParams] = useState(null);
+  const [params, setParams] = useState({ room: '', user: '' });
+  const [message, setMessage] = useState('');
+  const [isOpen, setOpen] = useState(false);
+  const [users, setUsers] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const searchParams = Object.fromEntries(new URLSearchParams(search));
@@ -16,11 +26,72 @@ const Chat = () => {
 
   useEffect(() => {
     socket.on('message', ({ data }) => {
-      console.log(data);
+      setState((_state) => [..._state, data]);
     });
   }, []);
 
-  return <div>Chat</div>;
+  useEffect(() => {
+    socket.on('room', ({ data: { users } }) => {
+      setUsers(users.length);
+    });
+  }, []);
+  const leftRoom = () => {
+    socket.emit('leftRoom', { params });
+    navigate('/');
+  };
+  const handleChange = ({ target: { value } }) => setMessage(value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!message) return;
+
+    socket.emit('sendMessage', { message, params });
+    setMessage('');
+  };
+  const onEmojiClick = ({ emoji }) => setMessage(`${message} ${emoji}`);
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.header}>
+        <div className={styles.title}>{params.room}</div>
+        <div className={styles.users}>{users} users in this room</div>
+        <button className={styles.left} onClick={leftRoom}>
+          Left the room
+        </button>
+      </div>
+      <div className={styles.messages}>
+        <Messages messages={state} name={params.name} />
+      </div>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.input}>
+          <input
+            placeholder="What do you want to say?"
+            type="text"
+            name="room"
+            className={styles.input}
+            autoComplete="off"
+            value={message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className={styles.emoji}>
+          <img src={icon} alt="" onClick={() => setOpen(!isOpen)} />
+
+          {isOpen && (
+            <div className={styles.emojies}>
+              <EmojiPicker onEmojiClick={onEmojiClick} />
+            </div>
+          )}
+        </div>
+        <div className={styles.button}>
+          <input type="submit" onSubmit={handleSubmit} value="Send a message" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
